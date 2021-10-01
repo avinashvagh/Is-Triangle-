4b38853afa61871ca40861c114c102632fc41622
Avinash Vagh <78526868+avinashvagh@users.noreply.github.com>
2021-10-02 01:50:29 +0530

    finally half done

---
+++ istriangle.js
@@ -0,0 +1,34 @@
+const inputs = document.querySelectorAll('.angle-input')
+const isTriangleBtn = document.querySelector('#is-triangle-btn')
+const outputEl = document.querySelector('#output');
+
+function calculateSumOfAngles(angle1, angle2, angle3) {
+    const sumOfAngles = angle1 + angle2 + angle3;
+    // console.log(sumOfAngles);
+    return sumOfAngles;
+}
+
+
+function isTriangle() {
+ const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
+ console.log(sumOfAngles);
+ if(sumOfAngles === 180) {
+    
+    outputEl.innerText = "Yay, The angles form a triangle";
+ }
+ else {
+   
+    outputEl.innerText = ("oh oh! The angle don't form a  triangle");
+ }
+}
+
+isTriangleBtn.addEventListener("click", isTriangle)
+
+
+
+
+
+
+
+
+
